8a40e9c91bfa04cc507cbeae46d93ae026fed410
lifeislearningforever <71548949+lifeislearningforever@users.noreply.github.com>
2022-09-20 22:22:34 +0800

    Update streamlit_app.py
    Added emojis and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
-streamlit.title('My parents new healthily dinner')
-streamlit.text('Adding the new text')
-streamlit.text('Checking the example')
-streamlit.text('Good going')
+streamlit.header('Breakfast Menu')
+streamlit.text('🍜Omega 3 & Blueberry Oatmeal')
+streamlit.text('🍲Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐓Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
