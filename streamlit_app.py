150bf08496f16ab6cafcc991324f43d6e94a3196
lifeislearningforever <71548949+lifeislearningforever@users.noreply.github.com>
2022-09-24 22:30:07 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,13 @@ import urllib.request
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
+  
+
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
@@ -24,10 +31,9 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get the information")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
-except URLError as e:
+    back_from_funcation=get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_funcation)
+ except URLError as e:
   streamlit.error()
 streamlit.write('The user entered ', fruit_choice)
 import requests
